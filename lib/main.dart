deb68156a23a15a054b05f60bdc6f0de2fd23a64
idikim <hjkim9217@naver.com>
2025-01-23 01:02:04 +0900

    add: UserListViewModel 구현 및 테스트
    - add: user provider 구현 및 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:wetravel/firebase_options.dart';
 import 'package:wetravel/presentation/pages/user_list/user_list_page.dart';
 
@@ -8,7 +9,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
