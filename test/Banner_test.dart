daba73b798d7c85bae4116614ae12c18b2f4016d
Min-woos <wcn67888@gmail.com>
2025-01-23 04:16:44 +0900

    feat: Banner_dto 테스트 구현

--- test/Banner_test.dart
+++ test/Banner_test.dart
@@ -1,26 +1,27 @@
+import 'dart:convert';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:wetravel/domain/entity/banner.dart';
 import 'package:wetravel/data/dto/banner_dto.dart';
 
 void main() {
-  group('Banner fromDto test', () {
-    test('should create Banner from BannerDto', () {
-      // Given
-      final bannerDto = BannerDto(
-        id: '1',
-        linkUrl: 'https://www.example.com',
-        // ... 나머지 필드 설정
-      );
+  test(
+    'BannerDto : fromJson test',
+    () {
+      const sampleJsonString = """
+{
+      "id": "banner_1",
+      "linkUrl": "https://www.example1.com",
+      "imageUrl": "https://example.com/banner1.jpg",
+      "company": "Example Company 1",
+      "description": "독보적인상품소개합니다!",
+      "startDate": "2025-01-01T00:00:00Z",
+      "endDate": "2025-01-31T23:59:59Z",
+      "isHidden": false,
+      "order": 1
+    }
+""";
 
-      // When
-      final banner = Banner.fromDto(bannerDto);
-
-      // Then
-      expect(banner.id, '1');
-      expect(banner.linkUrl, 'https://www.example.com');
-      // ... 나머지 필드 비교
-    });
-
-    // 다른 테스트 케이스 추가 (예: 비정상적인 데이터 입력 시 예외 발생 확인 등)
-  });
+      final banner = BannerDto.fromJson(jsonDecode(sampleJsonString));
+      expect(banner.company, "Example Company 1");
+    },
+  );
 }
