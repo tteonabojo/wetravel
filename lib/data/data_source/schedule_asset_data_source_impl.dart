e4716efcb3e245a0dd5424525a3c8c494a73a257
Min-woos <wcn67888@gmail.com>
2025-01-24 11:55:48 +0900

    feat: assetBundle 생성자 정의

--- lib/data/data_source/schedule_asset_data_source_impl.dart
+++ lib/data/data_source/schedule_asset_data_source_impl.dart
@@ -1,18 +1,15 @@
 import 'dart:convert';
 import 'package:flutter/services.dart';
 import 'package:wetravel/data/dto/schedule_dto.dart';
-import 'package:wetravel/data/data_source/schedule_data_source.dart';
 
-class ScheduleAssetDataSourceImpl implements ScheduleDataSource {
-  @override
+class ScheduleAssetDataSourceImpl {
+  final AssetBundle assetBundle;
+
+  ScheduleAssetDataSourceImpl({required this.assetBundle});
+
   Future<List<ScheduleDto>> getSchedules() async {
-    try {
-      // assets 폴더의 json 파일 읽기
-      final jsonString = await rootBundle.loadString('assets/data/schedules.json');
-      final List<dynamic> jsonList = json.decode(jsonString);
-      return jsonList.map((json) => ScheduleDto.fromJson(json)).toList();
-    } catch (e) {
-      rethrow;
-    }
+    final jsonString = await assetBundle.loadString('assets/data/schedules.json');
+    final List<dynamic> jsonData = jsonDecode(jsonString);
+    return jsonData.map((e) => ScheduleDto.fromJson(e)).toList();
   }
 }
