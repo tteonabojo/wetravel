dbb44d58d29e05fe26ae20d27442e6ce6fe9bd85
idikim <hjkim9217@naver.com>
2025-01-27 04:35:47 +0900

    fix: user repository implement test 파일 수정

--- test/data/repository/user_repository_impl_test.dart
+++ test/data/repository/user_repository_impl_test.dart
@@ -1,21 +1,35 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:wetravel/data/data_source/user_data_source.dart';
 import 'package:wetravel/data/dto/user_dto.dart';
 import 'package:wetravel/data/repository/user_repository_impl.dart';
 
+class MockFirebaseAuth extends Mock implements FirebaseAuth {}
+
+class MockGoogleSignIn extends Mock implements GoogleSignIn {}
+
 class MockUserDataSource extends Mock implements UserDataSource {}
+
+class MockFirebaseFirestore extends Mock implements FirebaseFirestore {}
 
 void main() {
   late final MockUserDataSource mockUserDataSource;
   late final UserRepositoryImpl userRepositoryImpl;
-  setUp(
-    () async {
-      mockUserDataSource = MockUserDataSource();
-      userRepositoryImpl = UserRepositoryImpl(mockUserDataSource);
-    },
-  );
+  late final MockFirebaseAuth mockFirebaseAuth;
+  late final MockGoogleSignIn mockGoogleSignIn;
+  late final MockFirebaseFirestore mockFirestore;
+
+  setUp(() {
+    mockUserDataSource = MockUserDataSource();
+    mockFirebaseAuth = MockFirebaseAuth();
+    mockGoogleSignIn = MockGoogleSignIn();
+    mockFirestore = MockFirebaseFirestore();
+    userRepositoryImpl = UserRepositoryImpl(
+        mockUserDataSource, mockFirebaseAuth, mockGoogleSignIn, mockFirestore);
+  });
   test(
     "UserRepositoryImpl test : fetchUser",
     () async {
