85290a1b7217484c6b4cb0b12f1829c8adcb9197
idikim <hjkim9217@naver.com>
2025-01-31 10:46:17 +0900

    bugfix: AppIcons 파일 오타 수정

--- lib/constants/app_icons.dart
+++ lib/constants/app_icons.dart
@@ -10,10 +10,10 @@ class AppIcons {
   // 내비게이션 바 아이콘
   static final SvgPicture home = SvgPicture.asset('assets/icons/home.svg');
   static final SvgPicture squarePlus =
-      SvgPicture.asset('assets/icons/square-plus.svg');
+      SvgPicture.asset('assets/icons/square_plus.svg');
   static final SvgPicture crown = SvgPicture.asset('assets/icons/crown.svg');
   static final SvgPicture userRound =
-      SvgPicture.asset('assets/icons/user-round.svg');
+      SvgPicture.asset('assets/icons/user_round.svg');
   //
   //
   // general 아이콘
@@ -23,27 +23,27 @@ class AppIcons {
       SvgPicture.asset('assets/icons/compass.svg');
   static final SvgPicture trash = SvgPicture.asset('assets/icons/trash.svg');
   static final SvgPicture chevronLeft =
-      SvgPicture.asset('assets/icons/chevron-left.svg');
+      SvgPicture.asset('assets/icons/chevron_left.svg');
   static final SvgPicture chevronRight =
-      SvgPicture.asset('assets/icons/chevron-right.svg');
+      SvgPicture.asset('assets/icons/chevron_right.svg');
   static final SvgPicture plus = SvgPicture.asset('assets/icons/plus.svg');
   static final SvgPicture pen = SvgPicture.asset('assets/icons/pen.svg');
   static final SvgPicture arrowDownUp =
-      SvgPicture.asset('assets/icons/arrow-down-up.svg');
+      SvgPicture.asset('assets/icons/arrow_down_up.svg');
   static final SvgPicture ellipsisVertical =
-      SvgPicture.asset('assets/icons/ellipsis-vertical.svg');
+      SvgPicture.asset('assets/icons/ellipsis_vertical.svg');
   static final SvgPicture eye = SvgPicture.asset('assets/icons/eye.svg');
-  static final SvgPicture eyeOff = SvgPicture.asset('assets/icons/eye-off.svg');
+  static final SvgPicture eyeOff = SvgPicture.asset('assets/icons/eye_off.svg');
   static final SvgPicture check = SvgPicture.asset('assets/icons/check.svg');
   static final SvgPicture mapPin =
-      SvgPicture.asset('assets/icons/map-pin.svg', height: 18);
+      SvgPicture.asset('assets/icons/map_pin.svg', height: 18);
   static final SvgPicture mail = SvgPicture.asset('assets/icons/mail.svg');
   static final SvgPicture mailFill =
       SvgPicture.asset('assets/icons/mail_fill.svg');
   static final SvgPicture star = SvgPicture.asset('assets/icons/star.svg');
   static final SvgPicture x = SvgPicture.asset('assets/icons/x.svg');
   static final SvgPicture chevronDown =
-      SvgPicture.asset('assets/icons/chevron-down.svg');
+      SvgPicture.asset('assets/icons/chevron_down.svg');
   static final SvgPicture chevronUp =
-      SvgPicture.asset('assets/icons/chevron-up.svg');
+      SvgPicture.asset('assets/icons/chevron_up.svg');
 }
