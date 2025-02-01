c1c0adf0515a75a8e8ebe003b47db80e29630f18
김서후 <ksh20531@gmail.com>
2025-02-01 16:39:03 +0900

    feat: 스플래시 구현 및 자동로그인 처리

--- lib/presentation/pages/main/widgets/main_banner.dart
+++ lib/presentation/pages/main/widgets/main_banner.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class MainBanner extends StatelessWidget {
@@ -11,7 +12,12 @@ class MainBanner extends StatelessWidget {
       width: double.infinity,
       color: Colors.black45,
       child: Center(
-        child: Text('banner'),
+        child: GestureDetector(
+            onTap: () {
+              // TODO:마이페이지에 로그아웃 기능 구현 후 삭제
+              FirebaseAuth.instance.signOut();
+            },
+            child: Text('banner')),
       ),
     );
   }
