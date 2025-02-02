35ac2fb94109583b742a8e770226ca889ff4f875
idikim <hjkim9217@naver.com>
2025-02-03 03:27:04 +0900

    fix: provider 수정

--- lib/presentation/provider/user_provider.dart
+++ lib/presentation/provider/user_provider.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:wetravel/data/data_source/data_source_implement/user_data_source_impl.dart';
 import 'package:wetravel/data/data_source/user_data_source.dart';
@@ -7,25 +8,45 @@ import 'package:wetravel/domain/repository/user_repository.dart';
 import 'package:wetravel/domain/usecase/fetch_user_usecase.dart';
 import 'package:wetravel/domain/usecase/sign_in_with_provider_usecase.dart';
 
-final _userDataSourceProvider = Provider<UserDataSource>((ref) {
-  return UserDataSourceImpl(FirebaseFirestore.instance);
+final _firebaseFirestoreProvider = Provider<FirebaseFirestore>((ref) {
+  return FirebaseFirestore.instance;
 });
 
-final _userRepositoryProvider = Provider<UserRepository>(
+final _firebaseAuthProvider = Provider<FirebaseAuth>((ref) {
+  return FirebaseAuth.instance;
+});
+
+final _userDataSourceProvider = Provider<UserDataSource>((ref) {
+  final firebaseFirestore = ref.watch(_firebaseFirestoreProvider);
+  return UserDataSourceImpl(firebaseFirestore);
+});
+
+final userRepositoryProvider = Provider<UserRepository>(
   (ref) {
     final dataSource = ref.watch(_userDataSourceProvider);
-    return UserRepositoryImpl(dataSource);
+    final firebaseFirestore =
+        ref.watch(_firebaseFirestoreProvider); // firestore 의존성 추가
+    final firebaseAuth =
+        ref.watch(_firebaseAuthProvider); // firebaseAuth 의존성 추가
+    return UserRepositoryImpl(dataSource, firebaseFirestore, firebaseAuth);
   },
 );
 
 final fetchUserUsecaseProvider = Provider(
   (ref) {
-    final userRepo = ref.watch(_userRepositoryProvider);
+    final userRepo = ref.watch(userRepositoryProvider);
     return FetchUserUsecase(userRepo);
   },
 );
 
 final signInWithProviderUsecaseProvider = Provider((ref) {
-  final userRepo = ref.read(_userRepositoryProvider);
+  final userRepo = ref.read(userRepositoryProvider);
   return SignInWithProviderUsecase(userRepo);
+});
+
+final isGuideProvider = FutureProvider<bool>((ref) async {
+  final fetchUserUsecase = ref.watch(fetchUserUsecaseProvider);
+  final user = await fetchUserUsecase.execute();
+
+  return user.isGuide;
 });
