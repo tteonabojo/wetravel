eabf669c46d6fd2683d388778cd99229a7407323
idikim <hjkim9217@naver.com>
2025-02-05 21:17:45 +0900

    fix: 가이드 추천패키지 리스트 수정중

--- lib/presentation/pages/guidepackage/widgets/filters.dart
+++ lib/presentation/pages/guidepackage/widgets/filters.dart
@@ -1,22 +1,41 @@
 import 'package:flutter/material.dart';
-import 'filter_chip.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:wetravel/presentation/provider/recommendation_provider.dart';
 
-class GuideFilters extends StatelessWidget {
+class GuideFilters extends ConsumerWidget {
+  const GuideFilters({super.key});
+
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 20.0),
-      color: Colors.white,
+  Widget build(BuildContext context, WidgetRef ref) {
+    final recommendationState = ref.watch(recommendationStateProvider);
+
+    // 선택한 키워드 및 도시 결합
+    final allKeywords = [
+      ...recommendationState.selectedCities, // 선택한 도시 추가
+      recommendationState.travelPeriod,
+      recommendationState.travelDuration,
+      ...?recommendationState.travelStyles,
+      ...?recommendationState.companions,
+      ...?recommendationState.accommodationTypes,
+    ]
+        .where((keyword) => keyword != null && keyword.toString().isNotEmpty)
+        .toList();
+
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
       child: SingleChildScrollView(
         scrollDirection: Axis.horizontal,
         child: Row(
-          children: [
-            GuideFilterChip(label: "도쿄"),
-            GuideFilterChip(label: "혼자"),
-            GuideFilterChip(label: "2박 3일"),
-            GuideFilterChip(label: "액티비티"),
-            GuideFilterChip(label: "게스트 하우스"),
-          ],
+          children: allKeywords
+              .map<Widget>(
+                (keyword) => Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                  child: Chip(
+                    label: Text(keyword.toString()),
+                  ),
+                ),
+              )
+              .toList(),
         ),
       ),
     );
