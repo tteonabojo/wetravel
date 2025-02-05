c43fda96d5f6ee77e8bf87b5dc34f81a7f043681
JINI093 <koubook.jin@gamil.com>
2025-02-05 17:47:40 +0900

    fix : package dto 복구

--- lib/presentation/pages/stack/widgets/custom_bottom_navigation_bar.dart
+++ lib/presentation/pages/stack/widgets/custom_bottom_navigation_bar.dart
@@ -2,42 +2,70 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:wetravel/core/constants/app_colors.dart';
 import 'package:wetravel/core/constants/app_icons.dart';
-import 'package:wetravel/presentation/pages/new_trip/new_trip_page.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class CustomBottomNavigationBar extends StatelessWidget {
-  final BuildContext context;
-  final WidgetRef ref;
   final int selectedIndex;
   final Function(int) onItemTapped;
+  final BuildContext context;
+  final WidgetRef ref;
 
   const CustomBottomNavigationBar({
-    required this.context,
-    required this.ref,
+    super.key,
     required this.selectedIndex,
     required this.onItemTapped,
-    super.key,
+    required this.context,
+    required this.ref,
   });
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      items: const <BottomNavigationBarItem>[
+      backgroundColor: Colors.white,
+      items: <BottomNavigationBarItem>[
         BottomNavigationBarItem(
-          icon: Icon(Icons.home_outlined),
+          icon: SvgPicture.asset(
+            AppIcons.home,
+            color: AppColors.grayScale_550,
+          ),
+          activeIcon: SvgPicture.asset(
+            AppIcons.home,
+            color: AppColors.primary_450,
+          ),
           label: '홈',
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.add_box_outlined),
-          label: '새 여행',
+          icon: SvgPicture.asset(
+            AppIcons.squarePlus,
+            color: AppColors.grayScale_550,
+          ),
+          activeIcon: SvgPicture.asset(
+            AppIcons.squarePlus,
+            color: AppColors.primary_450,
+          ),
+          label: '여행 시작',
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.bookmark_outline),
-          label: '내가 담은 패키지',
+          icon: SvgPicture.asset(
+            AppIcons.crown,
+            color: AppColors.grayScale_550,
+          ),
+          activeIcon: SvgPicture.asset(
+            AppIcons.crown,
+            color: AppColors.primary_450,
+          ),
+          label: '가이드',
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.person_outline),
-          label: '프로필',
+          icon: SvgPicture.asset(
+            AppIcons.userRound,
+            color: AppColors.grayScale_550,
+          ),
+          activeIcon: SvgPicture.asset(
+            AppIcons.userRound,
+            color: AppColors.primary_450,
+          ),
+          label: '마이페이지',
         ),
       ],
       currentIndex: selectedIndex,
@@ -47,8 +75,6 @@ class CustomBottomNavigationBar extends StatelessWidget {
       showSelectedLabels: false,
       showUnselectedLabels: false,
       type: BottomNavigationBarType.fixed,
-      selectedItemColor: Colors.blue,
-      unselectedItemColor: Colors.grey,
     );
   }
 }
