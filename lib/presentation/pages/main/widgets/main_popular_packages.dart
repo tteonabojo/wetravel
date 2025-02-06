0a0025d9f0f5eb2dc56df824a3022849ef3c877d
idikim <hjkim9217@naver.com>
2025-02-06 10:36:28 +0900

    fix: 메인페이지 스크롤방식 변경

--- lib/presentation/pages/main/widgets/main_popular_packages.dart
+++ lib/presentation/pages/main/widgets/main_popular_packages.dart
@@ -7,50 +7,44 @@ import 'package:wetravel/presentation/widgets/package_item.dart';
 class MainPopularPackages extends StatelessWidget {
   /// 메인 페이지 인기 패키지 영역
   final List<Package> popularPackages;
+
   const MainPopularPackages({super.key, required this.popularPackages});
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: MainLabel(label: '인기 있는 패키지'),
-          ),
-          Expanded(
-              child: popularPackages.isNotEmpty
-                  ? ListView.separated(
-                      padding: EdgeInsets.symmetric(horizontal: 16),
-                      itemCount: popularPackages.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        int? rate =
-                            index <= 2 ? index + 1 : null; // 3등 까지 순위 표시
-                        return SizedBox(
-                          child: PackageItem(
-                            rate: rate,
-                            title: popularPackages[index].title,
-                            location: popularPackages[index].location,
-                            guideImageUrl: popularPackages[index].userImageUrl,
-                            packageImageUrl: popularPackages[index].imageUrl,
-                            name: popularPackages[index].userName,
-                            keywords:
-                                popularPackages[index].keywordList!.toList(),
-                          ),
-                        );
-                      },
-                      separatorBuilder: (context, index) {
-                        return SizedBox(height: 8);
-                      },
-                    )
-                  : Center(
-                      child: Text(
-                        '인기있는 패키지가 없어요',
-                        style: AppTypography.body3,
-                      ),
-                    ))
-        ],
-      ),
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: MainLabel(label: '인기 있는 패키지'),
+        ),
+        popularPackages.isNotEmpty
+            ? Column(
+                spacing: 8,
+                children: List.generate(
+                  popularPackages.length,
+                  (index) {
+                    int? rate = index <= 2 ? index + 1 : null; // 3등 까지 순위 표시
+                    return PackageItem(
+                      rate: rate,
+                      title: popularPackages[index].title,
+                      location: popularPackages[index].location,
+                      guideImageUrl: popularPackages[index].userImageUrl,
+                      packageImageUrl: popularPackages[index].imageUrl,
+                      name: popularPackages[index].userName,
+                      keywords: popularPackages[index].keywordList!.toList(),
+                    );
+                  },
+                ),
+              )
+            : Center(
+                child: Text(
+                  '인기있는 패키지가 없어요',
+                  style: AppTypography.body3,
+                ),
+              ),
+        SizedBox(height: 16)
+      ],
     );
   }
 }
