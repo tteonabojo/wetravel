b11cc309cd4a928bbca4b593b36f8ed1e4a5a78e
idikim <hjkim9217@naver.com>
2025-02-17 18:10:06 +0900

    fix: 디자인 및 선택키워드 수정

--- lib/presentation/pages/schedule/ai_schedule_page.dart
+++ lib/presentation/pages/schedule/ai_schedule_page.dart
@@ -69,7 +69,6 @@ class _AISchedulePageState extends ConsumerState<AISchedulePage> {
                 return Center(child: Text('Error: ${snapshot.error}'));
               }
 
-              final schedule = snapshot.data!;
               return Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -81,13 +80,15 @@ class _AISchedulePageState extends ConsumerState<AISchedulePage> {
                   ),
                   Expanded(
                     child: Container(
-                      padding: const EdgeInsets.symmetric(horizontal: 16),
                       color: AppColors.grayScale_050,
                       child: Column(
                         children: [
-                          ScheduleDayTabs(
-                              dayCount:
-                                  _getDayCount(surveyResponse.travelDuration)),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(16, 20, 16, 0),
+                            child: ScheduleDayTabs(
+                                dayCount: _getDayCount(
+                                    surveyResponse.travelDuration)),
+                          ),
                           Expanded(
                             child: ScheduleList(
                               surveyResponse: surveyResponse,
