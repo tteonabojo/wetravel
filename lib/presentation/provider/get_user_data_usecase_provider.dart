17be5aac8748113fc132dd769c473724350171f3
Min-woos <wcn67888@gmail.com>
2025-02-26 05:33:13 +0900

    refactor: fetchUserData 메서드에서 직접 Firestore에 접근하여 필요없음

--- lib/presentation/provider/get_user_data_usecase_provider.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:wetravel/domain/repository/user_repository.dart';
-import 'package:wetravel/domain/usecase/get_user_data_usecase.dart';
-import 'package:wetravel/presentation/provider/user_provider.dart';
-
-final getUserDataUseCaseProvider = Provider<GetUserDataUseCase>((ref) {
-  final repository = ref.watch(userRepositoryProvider);
-  return GetUserDataUseCase(repository);
-});
